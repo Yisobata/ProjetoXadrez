463d6afe66207aba457a5f3b26dd7d82b6865b5b
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-02-01 19:16:17 -0300

    Criacao da primeira classe de Posicao.

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace ProjetoXadrez
 {
@@ -6,7 +7,11 @@ namespace ProjetoXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadLine();
         }
     }
 }
