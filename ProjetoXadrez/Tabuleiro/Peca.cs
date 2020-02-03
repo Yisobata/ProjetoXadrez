f80e6be5ea85182b4e26d0c49875183b053f0ad2
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-02-03 18:22:28 -0300

    Implementando as restricoes de movimento

--- ProjetoXadrez/Tabuleiro/Peca.cs
+++ ProjetoXadrez/Tabuleiro/Peca.cs
@@ -13,11 +13,32 @@
             this.cor = cor;
             this.tab = tab;
             this.qteMovimentos = 0;
-        }      
+        }
 
         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
         }
 
         public abstract bool[,] movimentosPossiveis();
---
+++ ProjetoXadrez/xadrez/Bizpo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using tabuleiro;
+
+namespace ProjetoXadrez.xadrez
+{
+    class Bizpo : Peca
+    {
+        public Bizpo(Tabuleiro tab, Cor cor)
+            : base(tab, cor)
+        {
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+    }
+}
