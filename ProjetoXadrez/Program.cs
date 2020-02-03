6cff373c32f4d71f9c55d770fb07f7402474dce1
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-02-03 14:57:44 -0300

    Colocando as pecas no tabuleiro.

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -16,9 +16,10 @@ namespace ProjetoXadrez
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
 
+                    Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                    Console.Write("Destino");
+                    Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
                     partida.executaMovimento(origem, destino);
